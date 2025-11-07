6bcc90fa98d3b3623f454b1ce233406f95c5ab19
seba <seba@localhost.localdomain>
2025-11-07 14:35:58 -0300

    add plot of the beams

--- example/panels_simulation.py
+++ example/panels_simulation.py
@@ -63,7 +63,7 @@ sigma_r = 0.002
 
 ##deformation hyperparameters
 key = jax.random.key(0)
-start_rms = 1e-4                ##this really is mu in a normal distribution
+start_rms = 60*1e-6                ##this really is mu in a normal distribution
 
 #
 batch_size = 256
@@ -145,33 +145,25 @@ E = forw_function(coeffs, sec_offset)
 E_host = jax.block_until_ready(E)
 print("Forward function took: {:.4f}".format((time.time()-start)))
 
+##plot the resulting beam maps
+E_out = E_host.reshape((target_points, target_points))
+u = np.linspace(-target_map_size/2, target_map_size/2, target_points)
+uv, vv = np.meshgrid(u,u)
 
-"""
+power = 20*np.log10(np.abs(E_out))
+phase = np.rad2deg(np.angle(E_out))
 
+fig, axes = plt.subplots(2,2)
+axes[0,0].pcolormesh(uv.to_value(apu.deg), vv.to_value(apu.deg), np.abs(E_out))
+axes[0,1].pcolormesh(uv.to_value(apu.deg), vv.to_value(apu.deg), phase)
 
-@partial(jax.jit,static_argnames=('batch_size','panels','s_pos0', 's_n',
-                                  's_ds', 'target_pos', 'horn_position',
-                                  'edge_tapper', 'horn_aperture', 'wavel',
-                                  'batch_size'))
-def forward_function(coeffs, sec_offset, panels, s_pos0, s_n, s_ds, 
-                    target_pos, 
-                     horn_position, edge_tapper, horn_aperture,
-                     wavel,
-                     batch_size
-                     ):
-    s_pos = s_pos0+sec_offset[None,:]
-    p_pos, p_n, p_ds = apply_panel_deformation(panels, coeffs)
-    E_i_kf = propagate_cylindrical_gaussian_beam(edge_tapper, horn_aperture, wavel, 
-                                                 horn_position, s_pos)
-    E_s_kf = jax.block_until_ready(kirchhoff_fresnel_scan(s_pos, -s_n, s_ds, E_i_kf, p_pos, wavel, chunk_size=batch_size))
-    E_p_k = jax.block_until_ready(kirchhoff_fresnel_scan(p_pos, p_n, p_ds, E_s_kf, target_pos, wavel, chunk_size=batch_size)) 
-    return E_p_k
+axes[1,0].plot(u.to_value(apu.deg), power[:, target_points//2], color='darkred')
+axes[1,0].plot(u.to_value(apu.deg), np.diag(power), color='darkblue')
+axes[1,0].grid()
 
+axes[1,1].plot(u.to_value(apu.deg), phase[:, target_points//2], color='darkred')
+axes[1,1].plot(u.to_value(apu.deg), np.diag(phase), color='darkblue')
+axes[1,1].grid()
 
-start = time.time()
-E = forward_function(coeffs, sec_offset, panels, s_pos, s_n, s_ds,
-                     target_pos, horn_position, edge_tapper.to_value(apu.dB),
-                     horn_aperture.to_value(apu.m), wavel.to_value(apu.m),
-                     batch_size)
+plt.show()
 
-"""
