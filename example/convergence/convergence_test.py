adc432b1d35e661e1355e65e6e99b71a4093d08b
seba <sjorquer@gmail.com>
2026-07-15 12:54:11 -0400

    add the convergence test code (still need to review results)

--- example/convergence/convergence_test.py
+++ example/convergence/convergence_test.py
@@ -14,6 +14,21 @@ from hyperparameters import *
 import h5py
 
 
+#to have a proper simulation we need the jnp.complex128 representation
+os.system("export JAX_ENABLE_X64=True")
+jax.config.update('jax_enable_x64', True)
+
+
+sec_offset = [0*apu.mm, 0*apu.mm, 15*apu.mm]
+
+r_points = 256
+t_points_primary = 512      ##angular points for the primary
+t_points_secondary = 512    ##angular points for the secondary
+
+##The idea of this script is to iterate over sampling to converge
+test_r_points = 256+np.arange(15)*52
+test_t_points_p = 512+np.arange(10)*52
+test_t_points_s = 512+np.arange(10)*52
 
 f = h5py.File("convergence_ouptut.hdf5", "w")
 dset = f.create_dataset(
@@ -29,17 +44,6 @@ dset = f.create_dataset(
 )
 
 
-sec_offset = [0*apu.mm, 0*apu.mm, 15*apu.mm]
-
-r_points = 256
-t_points_primary = 512      ##angular points for the primary
-t_points_secondary = 512    ##angular points for the secondary
-
-##The idea of this script is to iterate over sampling to converge
-test_r_points = 256+np.arange(15)*52
-test_t_points_p = 512+np.arange(10)*52
-test_t_points_s = 512+np.arange(10)*52
-
 
 for i in range(len(test_r_points)):
     for j in range(len(test_t_points_p)):
@@ -47,6 +51,7 @@ for i in range(len(test_r_points)):
             r_points = test_r_points[i]
             t_points_primary =  test_t_points_p[j]
             t_points_secondary = test_t_points_s[k]
+            print("r_points: %i t_prim: %i t_sec: %i"%(r_points, t_points_primary, t_points_secondary))
 
             panels, s_pos, s_n, s_ds, B, target_pos = create_apex_geometries(r_min_prim, d1, r_points,
                               r_min_sec, d2,
@@ -59,39 +64,41 @@ for i in range(len(test_r_points)):
                               )
 
             #convert the data into jnp arrays
-            sec_offset = jnp.array([x.to_value(apu.m) for x in sec_offset]).astype(jnp.float32)
-            s_pos = jnp.array(s_pos.to_value(apu.m)).astype(jnp.float32)
+            sec_offset_j = jnp.array([x.to_value(apu.m) for x in sec_offset]).astype(jnp.float32)
+            s_pos_j = jnp.array(s_pos.to_value(apu.m)).astype(jnp.float32)
             ##the target positions affect the dynamic range of the output..
             #target_pos = jnp.array(target_pos.to_value(apu.m)).astype(jnp.float64)
-            target_pos = jnp.array(target_pos.to_value(apu.m)).astype(jnp.float32)
+            target_pos_j = jnp.array(target_pos.to_value(apu.m)).astype(jnp.float32)
 
 
             panels = jax.tree_util.tree_map(lambda x: jnp.array(x, dtype=jnp.float32), panels)
             coeffs = generate_start_coeffs(key, panels.keys(), start_rms=start_rms, dtype=jnp.float32)
-            s_n = jnp.array(s_n).astype(jnp.float32)
-            s_ds = jnp.array(s_ds.to_value(apu.m**2)).astype(jnp.float32)
+            s_n_j = jnp.array(s_n).astype(jnp.float32)
+            s_ds_j = jnp.array(s_ds.to_value(apu.m**2)).astype(jnp.float32)
+            p_pos, p_n, p_ds, panel_ms = apply_panel_deformation(panels, coeffs)
 
-            horn_position = (jnp.array((0,0,B.to_value(apu.m))).T).astype(jnp.float32)
+            horn_position_j = (jnp.array((0,0,B.to_value(apu.m))).T).astype(jnp.float32)
 
             ###ok, here we start the pure jax shit
             def make_forward_function(panels, s_pos0, s_n, s_ds,
+                                p_pos, p_n, p_ds,
                                 target_pos, horn_position, edge_tapper, horn_aperture,
                                  wavel, batch_size):
                 @jax.jit
-                def forward_function(coeffs, sec_offset):
+                def forward_function(sec_offset):
                     s_pos = s_pos0+sec_offset[None,:]
-                    p_pos, p_n, p_ds, panel_ms = apply_panel_deformation(panels, coeffs)
                     E_i_kf = propagate_cylindrical_gaussian_beam(edge_tapper, horn_aperture, wavel,
                                                                  horn_position, s_pos)
                     E_s_kf = kirchhoff_fresnel_scan(s_pos, -s_n, s_ds, E_i_kf, p_pos, wavel, chunk_size=batch_size)
                     E_p_k = kirchhoff_fresnel_scan(p_pos, p_n, p_ds, E_s_kf, target_pos, wavel, chunk_size=batch_size)
                     return E_p_k
                 return forward_function
-            forw_function = make_forward_function(panels, s_pos, s_n, s_ds,
-                     target_pos, horn_position, edge_tapper.to_value(apu.dB),
+            forw_function = make_forward_function(panels, s_pos_j, s_n_j, s_ds_j,
+                    p_pos, p_n, p_ds,
+                     target_pos_j, horn_position_j, edge_tapper.to_value(apu.dB),
                      horn_aperture.to_value(apu.m), wavel.to_value(apu.m),
                      batch_size)
-            E = forw_function(coeffs, sec_offset)
+            E = forw_function(sec_offset_j)
             E_host = jax.block_until_ready(E)
             E_out = E_host.reshape((target_points, target_points))
             dset[:,:,i,j,k] = np.array(E_out)
