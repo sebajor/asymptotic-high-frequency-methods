de0d391698be80e8c4af2b0bf2989164c9236e94
seba <seba@localhost.localdomain>
2026-08-22 01:29:12 -0400

    add panel fit

--- apex_pipeline/panel_fit.py
+++ apex_pipeline/panel_fit.py
@@ -34,6 +34,13 @@ parser.add_argument('-cl', '--conv_lim', dest='conv_lim', type=float, default=4*
                     help="convergence limit")
 parser.add_argument("-ll", "--loss_lim", dest='loss_lim', type=float, default=3*1e-11,
                     help="lower limit of the loss")
+
+parser.add_argument("-gamma", "--gamma", dest='gamma', type=float, default=10,
+                    help="lagrange multiplier for the surface error")
+
+parser.add_argument("-geo_file", "--geo_file", dest='geo_file', type=str, default=None,
+                    help="Geometry file. If None and you set a hdf5 file as filename the code checks for it in the standard location")
+
 parser.add_argument("-pi","--plot_interval",dest='plot_interval', type=int, default=10,
                    help="How often generate the debugging plots")
 parser.add_argument("-plot_path", dest='plot_path', type=str, default="~/MODULES/physical_optics/")
@@ -74,7 +81,7 @@ if(filename.endswith('.reg')):
     F = phase_correction(F,u,v,wavel)
     F = np.conj(F)
     F = F/F[128,128]
-    ##TODO: check that this flip is correct!
+    ##TODO: check that the data is indeed rotated!
     F = F[::-1,::-1]
     plot_path = os.path.join(plot_dir, filename.split('.reg')[0])
 
@@ -83,7 +90,6 @@ elif(filename.endswith('.hdf5')):
     F = np.array(f['phase_correction']['data'])
     F = np.conj(F)
     F = F/F[128,128]
-    ##TODO: check that this flip is correct!
     F = F[::-1,::-1]
     plot_path = os.path.join(plot_dir, os.path.split(os.path.split(os.path.split(filepath)[0])[0])[1])
 
@@ -91,16 +97,30 @@ else:
     print("Unsuported input file.. must be .reg or .hdf5!")
     sys.exit(1)
 
+###get the geometry parameters
+if((args.geo_file is None) and filename.endswith('.hdf5')):
+    geo_path = os.path.join(os.path.split(os.path.split(filepath)[0])[0], 
+                            "geometry_fit", "geo_params.npz")
+    if(not os.path.exists(geo_path)):
+        print("Gemetry file not found!")
+        sys.exit(1)
+elif(not os.path.exists(os.path.expanduser(args.geo_file))):
+    print("Geometry file not given!")
+
+else:
+    geo_path = os.path.expanduser(args.geo_file)
+
+geo_params = np.load(geo_path, allow_pickle=1)['params'].tolist()
+
+
 print("Creating the plot directories at %s"%plot_path)
 os.makedirs(plot_path, exist_ok=True)
-plot_path = os.path.join(plot_path, 'geometry_fit')
+plot_path = os.path.join(plot_path, 'panel_fit')
 os.makedirs(plot_path, exist_ok=True)
 
-##computing the target aperture
-E_shift = np.fft.ifftshift(F)
-gold_aperture = np.fft.fftshift(np.fft.ifft2(E_shift))
 
 
+####
 ##set the 64 bit operations at the GPU (we need complex128 to resolve the phase
 ##at the SNR that we want)
 os.system("export JAX_ENABLE_X64=True")
@@ -108,7 +128,7 @@ jax.config.update('jax_enable_x64', True)
 
 
 learning_rate = args.learning_rate
-gamma = 1                   ##here we dont care.. the deformation rms is zero always
+gamma = args.gamma
 iters = args.max_iters
 loss_lim = args.loss_lim
 conv_lim = args.conv_lim
@@ -116,14 +136,15 @@ plot_interval = args.plot_interval
 
 map_dtype = jnp.complex128
 
+###
+### Here we start to define everything! Note that most of the hyperparams are in 
+### in the hyperparameter file!
+###
 
-###
-###Here we start to define everything! Note that the hyperparameters are in hyperparameters file!
-###
 panels, s_pos, s_n, s_ds, sec_vertex, B, target_pos = create_apex_geometries(r_min_prim, d1, r_points,
-                      r_min_sec, d2, 
+                      r_min_sec, d2,
                       t_points_primary, t_points_secondary,
-                      f1, f_d, 
+                      f1, f_d,
                       silhouette, legs_diameter, secondary_silhouette,
                       sigma_t, sigma_r,
                       target_distance, target_map_size, target_points,
@@ -131,41 +152,41 @@ panels, s_pos, s_n, s_ds, sec_vertex, B, target_pos = create_apex_geometries(r_m
                       )
 
 
+sec_offsets = jnp.array(geo_params['sec_offsets'], dtype=jnp.float32)
+sec_rotation = jnp.array(geo_params['sec_rotation'], dtype=jnp.float32)
+horn_offsets = jnp.array(geo_params['horn_offsets'], dtype=jnp.float32)
+horn_rotation = jnp.array(geo_params['horn_rotation'], dtype=jnp.float32)
+edge_tapper = jnp.array(geo_params['edge_tapper'], dtype=jnp.float32)
+horn_aperture = jnp.array(geo_params['horn_aperture'], dtype=jnp.float32)
+
+
 #convert the data into jnp arrays
-horn_offsets = jnp.array([x.to_value(apu.m) for x in horn_offsets]).astype(jnp.float32)
-horn_rotation = jnp.array([x.to_value(apu.rad) for x in horn_rotation]).astype(jnp.float32)
-sec_offsets = jnp.array([x.to_value(apu.m) for x in sec_offsets]).astype(jnp.float32)
-sec_rotation = jnp.array([x.to_value(apu.rad) for x in sec_rotation]).astype(jnp.float32)
-
-
 s_pos = jnp.array(s_pos.to_value(apu.m)).astype(jnp.float32)
 ##the target positions affect the dynamic range of the output..
 target_pos = jnp.array(target_pos.to_value(apu.m)).astype(jnp.float64)
-panels = jax.tree_util.tree_map(lambda x: jnp.array(x, dtype=jnp.float32), panels)
 
-##for this specific code this should be all zeros!
-coeffs = generate_start_coeffs_zeros(panels.keys(), dtype=jnp.float32)
+
+panels = jax.tree_util.tree_map(lambda x: jnp.array(x, dtype=jnp.float32), panels)
+coeffs = generate_start_coeffs(key, panels.keys(), start_rms=start_rms, dtype=jnp.float32)
 s_n = jnp.array(s_n).astype(jnp.float32)
 s_ds = jnp.array(s_ds.to_value(apu.m**2)).astype(jnp.float32)
-
 horn_position = (jnp.array((0,0,B.to_value(apu.m))).T).astype(jnp.float32)
-edge_tapper= jnp.array(edge_tapper.to_value(apu.dB))
-horn_aperture = jnp.array(horn_aperture.to_value(apu.m))
+
 
 
 ##create the forward function
-def make_forward_function(coeffs,panels, s_pos0, s_n0, s_ds, sec_vertex,
-                    target_pos, horn_position, 
-                     wavel, batch_size, target_distance, map_dtype=jnp.complex128):
+def make_forward_function(panels, s_pos0, s_n0, s_ds, sec_vertex,
+                    target_pos, horn_position,
+                    sec_offsets, sec_rotation,
+                    edge_tapper, horn_aperture,
+                    horn_offsets, horn_rotation,
+                    wavel, batch_size, target_distance, map_dtype=jnp.complex128):
     @jax.jit
-    def forward_function( 
-                         edge_tapper, horn_aperture, 
-                         horn_offsets, horn_rotation,
-                         sec_offsets, sec_rotation):
+    def forward_function(coeffs):
         s_pos, s_n = secondary_position_update(s_pos0, s_n0, sec_vertex, sec_offsets, sec_rotation)
         p_pos, p_n, p_ds, deform_ms = apply_panel_deformation(panels, coeffs)
 
-        E_i_kf = propagate_cylindrical_gaussian_beam_offset(edge_tapper, horn_aperture, horn_offsets, 
+        E_i_kf = propagate_cylindrical_gaussian_beam_offset(edge_tapper, horn_aperture, horn_offsets,
                                                             horn_rotation, wavel, horn_position, s_pos)
         ##to avoid store intermidiate states, the memory blows up
         E_s_kf = kirchhoff_fresnel_scan_remat(s_pos, -s_n, s_ds, E_i_kf, p_pos, wavel, chunk_size=batch_size, dtype=map_dtype)
@@ -176,97 +197,79 @@ def make_forward_function(coeffs,panels, s_pos0, s_n0, s_ds, sec_vertex,
     return forward_function
 
 
-forw_function = make_forward_function(coeffs, panels, s_pos, s_n, s_ds, sec_vertex.to_value(apu.m),
-                     target_pos, horn_position, wavel.to_value(apu.m),
+
+
+
+##create the forward function
+forw_function = make_forward_function(panels, s_pos, s_n, s_ds, sec_vertex.to_value(apu.m),
+                     target_pos, horn_position,
+                     sec_offsets, sec_rotation,
+                     edge_tapper, horn_aperture,
+                     horn_offsets, horn_rotation,
+                     wavel.to_value(apu.m),
                      batch_size, target_distance.to_value(apu.m),
-                     map_dtype=map_dtype)
+                     map_dtype = map_dtype
+                     )
 
 
 #def loss_funct(coeffs, sec_rotation, sec_offsets, gold, gamma):
 def loss_funct(params, gold, gamma, norm_point=256*128+128):
-    pred, deform_mse = forw_function(
-                                     params['edge_tapper'], params['horn_aperture'], 
-                                     params['horn_offsets'], params['horn_rotation'],
-                                     params['sec_offsets'], params['sec_rotation'])
+    pred, deform_mse = forw_function(params['coeffs'])
+    ##normalize the predictions
+    #error = pred-gold
     #pred_norm = pred/jnp.max(jnp.abs(pred))
     pred_norm = pred/pred[norm_point]
-    #error = pred_norm-gold
-    #
-    #this one is in the aperture
-    pred_reshape = pred_norm.reshape((256,256))
-    pred_shift = jnp.fft.ifftshift(pred_reshape)
-    aperture = jnp.fft.fftshift(jnp.fft.ifft2(pred_shift)).flatten()
-    error = aperture-gold
-    ##we use the same loss either way
+    error = pred_norm-gold
+    #loss in the aperture
+    #pred_reshape = pred.reshape((256,256)) #just to test
+    #pred_shift = jnp.fft.ifftshift(pred_reshape)
+    #aperture = jnp.fft.fftshift(jnp.fft.ifft2(pred_shift)).flatten()
+    #error = aperture-gold
     loss = jnp.mean(error.real**2+error.imag**2)+gamma*jnp.mean(deform_mse)
     #jax.debug.print("jax.debug.print(y) -> {y}", y=jnp.mean(deform_mse))
     return loss
 
 params = {
-        #"coeffs":coeffs,
-        "sec_rotation": sec_rotation,
-        "sec_offsets": sec_offsets,
-        'edge_tapper': edge_tapper,
-        'horn_aperture': horn_aperture,
-        'horn_offsets': horn_offsets,
-        'horn_rotation': horn_rotation
+        "coeffs":coeffs
         }
 
 loss_grad = jax.jit(jax.value_and_grad(loss_funct))
 optimizer = optax.adam(learning_rate)
+
 opt_state = optimizer.init(params)
 
+
 @jax.jit
-def train_step(params, opt_state, gold, gamma):
+def train_step(params, opt_state,  gold, gamma):
     loss, grads = loss_grad(params, gold, gamma)
     updates, opt_state = optimizer.update(grads, opt_state, params) ##sec_rotation (?)
     params = optax.apply_updates(params, updates)
     return params, opt_state, loss
 
-
-####
-####
-####
-def plot_debug(params, gold_aperture, F, target_points, plot_path, iteration):
+###
+###
+###
+def plot_debug(params, F, target_points, plot_path, iteration):
     fig, ax = plt.subplots(2,2,figsize=(10,10))
-    pred, deform_mse = forw_function(
-                                     params['edge_tapper'], params['horn_aperture'],
-                                     params['horn_offsets'], params['horn_rotation'],
-                                     params['sec_offsets'], params['sec_rotation'])
+    plot_deformations(panels, params['coeffs'],ax=ax[0,0], correct_global=0)
+
+    pred, deform_mse = forw_function(params['coeffs'])
     F_pred = pred/pred[257*128]
     F_pred = np.array(F_pred).reshape((target_points, target_points))
-
-    ax[0,0].plot(20*np.log10(np.abs(F_pred[:,128])), color='darkblue')
-    ax[0,0].plot(20*np.log10(np.abs(F[:,128])), color='darkred')
-    ax[0,0].set_ylim(-82, 2)
+    error = F-F_pred
+    beam_error = np.mean(error.real**2+error.imag**2)
+    surf_error = np.mean(deform_mse)
 
     ax[0,1].plot(20*np.log10(np.abs(np.diag(F_pred))), color='darkblue')
     ax[0,1].plot(20*np.log10(np.abs(np.diag(F))), color='darkred')
-    ax[0,1].set_ylim(-82, 2)
+    ax[0,1].set_ylim(-85, 5)
 
     E_shift = np.fft.ifftshift(F_pred)
     aperture = np.fft.fftshift(np.fft.ifft2(E_shift))
     ax[1,0].imshow(np.angle(aperture))
     ax[1,1].imshow(np.abs(aperture))
     title = "iteration "+str(iteration)+"\n"
-    title += "suref_pos:%.5f %.5f %.5f"%(params['sec_offsets'][0]*1e3,
-                               params['sec_offsets'][1]*1e3,
-                               params['sec_offsets'][2]*1e3)+"  "
-
-    title += "subref_rotation: %.5f %.5f %.5f"%(
-            np.rad2deg(params['sec_rotation'][0])*1e3,
-            np.rad2deg(params['sec_rotation'][1])*1e3,
-            np.rad2deg(params['sec_rotation'][2])*1e3)+"\n"
-    title += "horn pos: %.5f %.5f %.5f"%(params['horn_offsets'][0]*1e3,
-                                         params['horn_offsets'][1]*1e3,
-                                         params['horn_offsets'][2]*1e3
-            )+"   "
-    title += "horn rotation: %.5f %.5f %.5f"%(np.rad2deg(params['horn_rotation'][0])*1e3,
-                                         np.rad2deg(params['horn_rotation'][1])*1e3,
-                                         np.rad2deg(params['horn_rotation'][2])*1e3
-            )+"\n"
-    title += "edge tapper %.4f horn aperture %.4f"%(params['edge_tapper'], params['horn_aperture'])
-    #ax.set_title("iteration "+str(i))
+    title += "beam error: %E  surf error:%E"%(beam_error, surf_error)
     fig.suptitle(title)
     fig.savefig(os.path.join(plot_path, str(i)))
     plt.close(fig)
@@ -277,10 +280,6 @@ def pytree_to_numpy(tree):
             lambda x: np.array(x) if isinstance(x, jnp.ndarray) else x,
             tree)
 
-
-
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s")
@@ -288,31 +287,27 @@ if __name__ == '__main__':
     print("Starting Geometrical fitting")
     for i in range(args.max_iters):
         start = time.time()
-        params, opt_state, loss = train_step(params, opt_state, gold_aperture.flatten(),gamma)
+        params, opt_state, loss = train_step(params, opt_state, F.flatten(), gamma)
         losses.append(loss)
         log_msg = "iter:%i \t loss:%E \t time:%.3f "%(i, loss, time.time()-start)
         logging.info(log_msg)
         if(i%plot_interval==0):
-            plot_debug(params, gold_aperture, F, target_points, plot_path, i)
+            plot_debug(params, F, target_points, plot_path, iteration)
         if(np.mean(losses[-10:]) < loss_lim):
             print("Loss limit reached at iteration %i: %E < %E"%(i, loss, loss_lim))
             break
         if(np.mean(np.abs(np.diff(losses[-10:])))< conv_lim):
             print("Loss convergence reached at iteration %i: %E < %E"%(i, loss, np.mean(np.diff(losses[-10:]))))
             break
-
-    print("Out of the optimization loop")
+    print("Out if the optimization loop")
     out_params = pytree_to_numpy(params)
-    pred, deform_mse = forw_function(
-                                     params['edge_tapper'], params['horn_aperture'],
-                                     params['horn_offsets'], params['horn_rotation'],
-                                     params['sec_offsets'], params['sec_rotation'])
+    pred, deform_mse = forw_function(params['coeffs'])
     F_pred = pred/pred[257*128]
     F_pred = np.array(F_pred).reshape((target_points, target_points))
-
-    np.savez(os.path.join(plot_path, "geo_params.npz"),
-                params=out_params,
-                losses=losses,
-                F_pred=F_pred,
-                F_gold=F)
+    np.savez(os.path.join(plot_path, "panles_params.npz"),
+            params=out_params,
+            losses=losses,
+            gamma= gamma,
+            F_pred=F_pred,
+            F_gold=F)
 
